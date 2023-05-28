f7db79533117f01004d0e7ad0fb31b2953c2f381
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-05-28 20:47:35 +0200

    ionic live-update add ... --channel-name="MeineErsteDestination" ...

--- src/app/konstanten.ts
+++ src/app/konstanten.ts
@@ -1,5 +1,5 @@
 // Diese Klasse wird vom Skript UpdateCommitZeitstempel.js erzeugt
 export abstract class Konstanten {
 
-     static readonly ZEITSTEMPEL_COMMIT = "2023-05-28T18:36:49+02:00";
+     static readonly ZEITSTEMPEL_COMMIT = "2023-05-28T20:46:34+02:00";
 }
