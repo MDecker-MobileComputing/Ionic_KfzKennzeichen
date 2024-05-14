03fb1dcb1340ac142f984af09c7bb4a53829adea
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-05-14 11:59:55 +0200

    UpdateVersion.js gleich noch weiter verbessert

--- UpdateVersion.js
+++ UpdateVersion.js
@@ -12,12 +12,12 @@ const fs     = require('fs');
 // Quellcode-Datei, in der Konstante mit anzuzeigendem Wert definiert ist
 const zieldatei = "src/app/konstanten.ts";
 
-
 const zeitstempel = moment().format(); // Beispielausgabe: 2023-05-28T13:25:13+02:00
 
 const inhalt =  "// Diese Klasse wird vom Skript UpdateCommitZeitstempel.js erzeugt.\n" +
-                "export abstract class Konstanten {\n\n"                               +
-                `     static readonly ZEITSTEMPEL_COMMIT = \"${zeitstempel}\";\n`      +
+                "// Aktualisierung mit: npm run bump-version\n"                         +
+                "export abstract class Konstanten {\n\n"                                +
+                `     static readonly ZEITSTEMPEL_COMMIT = \"${zeitstempel}\";\n`       +
                 "}";
 
 const callbackFunktion = (fehlerObj) => {
@@ -28,7 +28,7 @@ const callbackFunktion = (fehlerObj) => {
 
       } else {
 
-          console.log( "\nDatei wurde erstellt:\n" + inhalt + "\n" )
+          console.log( "\nDatei wurde erstellt:\n\n" + inhalt + "\n" )
       }
 };
 
--- src/app/konstanten.ts
+++ src/app/konstanten.ts
@@ -1,5 +1,6 @@
-// Diese Klasse wird vom Skript UpdateCommitZeitstempel.js erzeugt
+// Diese Klasse wird vom Skript UpdateCommitZeitstempel.js erzeugt.
+// Aktualisierung mit: npm run bump-version
 export abstract class Konstanten {
 
-     static readonly ZEITSTEMPEL_COMMIT = "2024-05-14T11:57:10+02:00";
+     static readonly ZEITSTEMPEL_COMMIT = "2024-05-14T11:59:40+02:00";
 }
